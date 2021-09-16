710d38c3407d4f296347a7db48fd934171df3494
appleseed-iii <appleseed-iii@protonmail.com>
2021-09-16 16:41:37 -0500

    svg ohm

--- src/components/CompositorV4.js
+++ src/components/CompositorV4.js
@@ -33,7 +33,8 @@ import "cropperjs/dist/cropper.css";
 
 import "./stake.scss";
 
-import sOhm from '../assets/token_sOHM.png';
+// import sOhm from '../assets/token_sOHM.png';
+import sOhm from '../assets/token_Ohm.svg';
 import classifyImage, {classifyOhmieImage} from "../helpers/classifyImage";
 import {drawFinalCanvas} from "../helpers/drawCanvas.js";
 import {getViewWidth} from "../helpers/index.js";
